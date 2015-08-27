a2c676e2776bab5f899d5d72a361aebc85642573
Mike Graves <mgraves@mit.edu>
2015-08-27 11:49:10 -0400

    Use python packaging
    Closes #24. Project is now installable as a package. This exposes
    the pipeline script as `oastats`.

---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+OAStats Pipeline
+"""
+
+import io
+from setuptools import find_packages, setup
+
+
+with io.open('LICENSE') as f:
+    license = f.read()
+
+
+setup(
+    name='OAStats Pipeline',
+    version='0.1.0',
+    description='Generate download statistics for the Open Access collection',
+    long_description=__doc__,
+    url='https://github.com/MITLibraries/oastats-backend',
+    license=license,
+    author='Mike Graves',
+    author_email='mgraves@mit.edu',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'PyYAML',
+        'apache-log-parser',
+        'arrow',
+        'click',
+        'futures',
+        'geoip2',
+        'maxminddb',
+        'pycountry',
+        'pymongo',
+        'pysolr',
+        'python-dateutil',
+        'requests',
+        'six',
+        'ua-parser',
+        'user-agents',
+    ],
+    extras_requires={
+        ':python_version=="2.7"': ['ipaddr'],
+    },
+    entry_points={
+        'console_scripts': [
+            'oastats = pipeline.cli:main',
+        ]
+    },
+    classifiers=[
+        'Internal :: Do Not Upload',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
