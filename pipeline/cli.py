c805170947c33e1fa79bdf5299c2e19c71a4a082
Mike Graves <mgraves@mit.edu>
2016-11-18 13:53:08 -0500

    Add documentation

--- pipeline/cli.py
+++ pipeline/cli.py
@@ -58,6 +58,43 @@ def main():
 @click.option('--dspace', default='https://dspace.mit.edu/ws/oastats')
 @click.option('--database', envvar='OASTATS_DATABASE')
 def pipeline(files, month, geo_ip, dspace, database):
+    """
+    Process the Apache logs and populate the database with identities.
+
+    This command will process the logs and print the output to STDOUT. The
+    output format is CSV suitable for passing to PostGres's COPY command.
+    The field order is: status, country, url, referer, user_agent, datetime,
+    document_id. Any requests which could not be processed due to malformed
+    log entries will be logged to STDERR.
+
+    IP addresses are converted to three letter country codes using the
+    `GeoLite2 country database
+    <http://dev.maxmind.com/geoip/geoip2/geolite2/>`_. Make sure to use the
+    binary format (``.mmdb``) and that it's current; these are updated
+    regularly. Pass the location of this file using the ``--geo-ip`` option.
+
+    The pipeline can filter for log entries by date. Use the ``--month/-m``
+    option to specify a month to select. This can be repeated as many times
+    as desired to collect more than one month of requests. The format should
+    be the same as appears in the log entries, specifically, ``MMM/YYYY``.
+    If no month is provided all log entries will be processed.
+
+    Identity data is collected from a custom Dspace identity bitstream. This
+    can be specified using the ``--dspace`` option.
+
+    The path to one or more log files should be passed as arguments to the
+    pipeline. For example::
+
+        \b
+        (oastats)$ oastats pipeline -m Sep/2016 -m Oct/2016 \\
+            --geo-ip data/GeoLite2.mmdb \\
+            logs/2016/{09,10}/access.log 2>errors.log | output.csv
+        (oastats)$ psql -d database -c "COPY requests (status, country, \\
+            url, referer, user_agent, datetime, document_id) FROM STDIN \\
+            WITH CSV" < output.csv
+
+    """
+
     if not month:
         month = []
     dates = [arrow.get(d, ['MMM/YYYY', 'MMM-YYYY']) for d in month]
@@ -90,6 +127,23 @@ def pipeline(files, month, geo_ip, dspace, database):
 @click.option('--mongo-db', default='oastats')
 @click.option('--mongo-coll', default='summary')
 def summary(database, mongo, mongo_db, mongo_coll):
+    """
+    Create the summary collection in Mongo.
+
+    The current OAStats website uses a ``summary`` collection in Mongo which
+    effectively functions as a pregenerated query cache. This command will
+    generate and insert the necessary JSON objects into Mongo.
+
+    Though not required, it is recommended to create a temporary summary
+    collection in Mongo and rename it to ``summary`` once this command has
+    finished. For example::
+
+        \b
+        (oastats)$ oastats summary --mongo-coll summary_new
+        (oastats)$ mongo oastats --eval \\
+            'db.summary_new.renameCollection("summary", true)'
+    """
+
     engine.configure(database)
     client = pymongo.MongoClient(mongo)
     collection = client[mongo_db][mongo_coll]
@@ -111,6 +165,19 @@ def summary(database, mongo, mongo_db, mongo_coll):
 @click.argument('command', type=click.Choice(['create', 'drop']))
 @click.option('--database', envvar='OASTATS_DATABASE')
 def db(command, database):
+    """
+    Create/drop the PostGres database tables.
+
+    This will create or drop the database tables depending on which command
+    is provided (``create`` or ``drop``). Make sure the database exists
+    first.
+
+    Example::
+
+        (oastats)$ oastats db create
+
+    """
+
     engine.configure(database)
     if command == 'create':
         metadata.create_all(engine())
@@ -125,6 +192,23 @@ def db(command, database):
 @click.option('--mongo-db', default='oastats')
 @click.option('--mongo-coll', default='requests')
 def load(database, mongo, mongo_db, mongo_coll):
+    """
+    Load the Mongo requests collection into PostGres.
+
+    The entire Mongo requests collection will be iterated over and loaded
+    into PostGres. The collection is sorted by time descending before being
+    iterated. This is done in order to get the most recent (and complete)
+    identitiy data from the denormalized Mongo database. It is recommended
+    to make sure the requests collection has a descending index on the time
+    field before running::
+
+        \b
+        (oastats)$ mongo oastats --eval \\
+            "db.requests.createIndex({time: -1})"
+        (oastats)$ oastats load
+
+    """
+
     engine.configure(database)
     client = pymongo.MongoClient(mongo)
     collection = client[mongo_db][mongo_coll]
